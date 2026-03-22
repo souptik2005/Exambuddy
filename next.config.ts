af7160143c8beb5a7e9d5dee346e750a2637f322
Souptik dey <souptikdey2005@gmail.com>
2026-03-22 10:12:03 +0530

    config fix

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
-const nextConfig = {
-  webpack: (config) => {
+import type { NextConfig } from "next";
+import type { Configuration } from "webpack";
+
+const nextConfig: NextConfig = {
+  webpack: (config: Configuration) => {
+    config.resolve = config.resolve || {};
+    config.resolve.alias = config.resolve.alias || {};
     config.resolve.alias.canvas = false;
+
     return config;
   },
   turbopack: {},
